a0198fd17391fd72a8efab510d38813d7dbf8f63
myriam-charfeddine <myriam.charfeddine@gmail.com>
2024-12-05 02:08:49 +0100

    update: ner added

--- gradio_app.py
+++ gradio_app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from theme_classifier import theme_classifier
 import pandas as pd
+from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 
 def get_themes(theme_list_str,subtitles_path,save_path):
     theme_list = theme_list_str.split(',')
@@ -24,12 +25,22 @@ def get_themes(theme_list_str,subtitles_path,save_path):
     #     width=500,
     #     height=260
     # )
-
     return output_df.sort_values(by="Score", ascending=False)
+
+def get_character_network(subtitles_path, ner_save_path):
+    ner = NamedEntityRecognizer()
+    ner_df = ner.get_all_ners_inference(subtitles_path, ner_save_path)
+
+    character_network_generator = CharacterNetworkGenerator()
+    relationship_df = character_network_generator.character_network_generator(ner_df)
+    html = character_network_generator.draw_network_graph(relationship_df)
+
+    return html
 
 def main():
   with gr.Blocks() as iface:
-    #Section for Theme Classification 
+
+    #Theme Classification Section
     with gr.Row():
       with gr.Column():
         gr.HTML('<h1>Theme Classification : Zero Shot Classifiers</h1>')
@@ -39,11 +50,25 @@ def main():
             df_output = gr.DataFrame()
           with gr.Column():
             themes_list = gr.Textbox(label = "Themes")
-            subtitles_path = gr.Textbox(label = "Subtitles or script Path")
+            subtitles_path = gr.Textbox(label = "Subtitles")
             save_path = gr.Textbox(label = "Save Path")
             get_themes_button = gr.Button("Get Themes")
-            # get_themes_button.click(get_themes, inputs=[themes_list, subtitles_path, save_path], outputs=[plot])   
+            # get_themes_button.click(get_themes, inputs=[themes_list, subtitles_path, save_path], outputs=[plot]) 
             get_themes_button.click(get_themes, inputs=[themes_list, subtitles_path, save_path], outputs=[df_output]) 
+
+    #Character Network Section 
+    with gr.Row():
+      with gr.Column():
+        gr.HTML('<h1>Character Network : NERs and Graph</h1>')
+        with gr.Row():
+          with gr.Column():
+            network_html = gr.HTML()
+          with gr.Column():
+            subtitles_path = gr.Textbox(label = "Subtitles")
+            ner_save_path = gr.Textbox(label = "NERs Save Path")
+            network_generator_button = gr.Button("Get Character Network")
+            network_generator_button.click(get_character_network, inputs=[subtitles_path, ner_save_path], outputs=[network_html]) 
+          
           
             
   iface.launch(share=True)
